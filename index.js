e62787ef604e0886cf88f4f1b512a9a9f44bfd0c
Evgeniy Sumaev <9364016@gmail.com>
2023-05-25 19:35:07 +0300

    fix

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ bot.on("message", async (msg) => {
 
 app.post("/web-data", async (req, res) => {
   const { queryId, products = [], totalPrice } = await req.body;
-  res.json({ req });
+  return res.status(200).json({ req });
   // console.log({ req });
   // try {
   //   await bot.answerWebAppQuery(queryId, {
