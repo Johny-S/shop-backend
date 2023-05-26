e5163615ac1f03f0298d92fbc51e2fd90b37eb96
Evgeniy Sumaev <9364016@gmail.com>
2023-05-26 11:26:13 +0300

    fix

--- index.js
+++ index.js
@@ -61,22 +61,22 @@ bot.on("message", async (msg) => {
 
 app.post("/web-data", async (req, res) => {
   const { queryId, products = [], totalPrice } = req.body;
-  console.log(req.body);
-  // try {
-  //   await bot.answerWebAppQuery(queryId, {
-  //     type: "article",
-  //     id: queryId,
-  //     title: "Успешная покупка",
-  //     input_message_content: {
-  //       message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products
-  //         .map((item) => item.title)
-  //         .join(", ")}`,
-  //     },
-  //   });
-  return res.status(200).json(req.body);
-  // } catch (e) {
-  //   return res.status(500).json({});
-  // }
+  // console.log(req.body);
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: "article",
+      id: queryId,
+      title: "Успешная покупка",
+      input_message_content: {
+        message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products
+          .map((item) => item.title)
+          .join(", ")}`,
+      },
+    });
+    return res.status(200).json({});
+  } catch (e) {
+    return res.status(500).json({});
+  }
 });
 
 const PORT = "8000";
